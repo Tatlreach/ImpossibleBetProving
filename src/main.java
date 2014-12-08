dc6a4d4bbd429dfa17f90df9a94d1578ef15a8ea
Michael S <Tatlreach@gmail.com>
2014-12-08 15:47:55 -0600

    Added an adjust able variable for the number of turns a player can have.  Cleaned out some commented out code.

--- src/main.java
+++ src/main.java
@@ -6,25 +6,29 @@ import java.util.Random;
 public class main {
 
 	public static void main(String[] args) {
-		float trialCount = 30;
+		float trialCount = 10000;
+		int playerCount = 100;
+		int turnCount = 50;
 		float successCount = 0;
 		
 		for(int i = 1; i <= trialCount; i++){
-			if(impossibleGambling(100, 50)){
+			if(impossibleGambling(playerCount, turnCount)){
 				successCount++;
 			}
 		}
 
-		//float successRate = (successCount/trialCount);
-		System.out.println("successRate: "+successCount+"/"+trialCount);
+		float successRate = (successCount/trialCount);
+		System.out.println("successRate: "+successRate);//successCount+"/"+trialCount);
 	}
+	
+	
 	
 	public static boolean impossibleGambling(int playerCount, int turnCount){
 		ArrayList<Integer>	boxes = new ArrayList<>();
 		ArrayList<Integer>	playerNumbers = new ArrayList<>();
 		
 
-		for(int i =0; i <= playerCount; i++){
+		for(int i =0; i < playerCount; i++){
 			boxes.add(i);
 			playerNumbers.add(i);
 		}
@@ -36,7 +40,7 @@ public class main {
 		
 		
 		for( Integer playeNumber : playerNumbers ){
-			if( !followTheirNumber( playeNumber, boxes) ){
+			if( !followTheirNumber( playeNumber, boxes, turnCount) ){
 				return false;
 			}
 		}
@@ -49,13 +53,10 @@ public class main {
 	
 	
 	
-	public static boolean followTheirNumber( int playerNumber, ArrayList<Integer> boxes ){
+	public static boolean followTheirNumber( int playerNumber, ArrayList<Integer> boxes, int turnCount ){
 		int currentLookingBox = playerNumber;
-		int turns = 50;
+		int turns = turnCount;
 		int currentLookingBoxValue;
-		//ArrayList<Integer> availableBoxes = new ArrayList<>(playerNumbers);
-		//Random randomGenerator = new Random();
-		//randomGenerator.nextInt(boxes.size());
 		
 		while( turns > 0 ){
 			currentLookingBoxValue = boxes.get(currentLookingBox);
@@ -63,16 +64,9 @@ public class main {
 				//System.out.println("SUCCESS: playerNumber "+playerNumber+", currentLookingBox: "+currentLookingBox+", currentLookingBoxValue: "+currentLookingBoxValue);
 				return true;
 			}
-			
-			//availableBoxes.remove(currentLookingBox);
 
 			//System.out.println("FAILURE: currentLookingBox: "+currentLookingBox+", currentLookingBoxValue: "+currentLookingBoxValue);
 			currentLookingBox = currentLookingBoxValue;
-			
-//			while(currentLookingBoxValue < 0){
-//				currentLookingBoxValue = randomGenerator.nextInt(boxes.size());
-//			}
-//			
 			turns--;
 		}
 		
